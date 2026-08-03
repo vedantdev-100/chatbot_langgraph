57fc9a30a601eb6b655f7ba235bcb67dc3c96ff8
Sahil22Sharma <sahil.s.sharma22@gmail.com>
2026-08-03 18:22:13 +0530

    gh

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 with st.chat_message('user'):
     st.text("hi")
+    
+with st.chat_message('assistant'):
+    st.text("how can I help you?")
+    
+user_input = st.chat_input("Type here") 
+
+if user_input:
+    with st.chat_message('user'):
+        st.text(user_input)
